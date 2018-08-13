f68fa913254cff3b4eefc0b654ef324a9a012af1
David Graham <david.malcom.graham@gmail.com>
2018-08-13 09:08:10 -0600

    Add anchor similarity match
    An anchor is text used to increase the score for input lines most
    similar to it. The anchor reference point is typically the path to the
    current file in a vim buffer. So nearby files in the directory structure
    will rank higher in the result list.

--- src/lib.rs
+++ src/lib.rs
@@ -1,39 +1,83 @@
 extern crate regex;
 use regex::{escape, Regex, RegexBuilder};
 
+mod anchor;
 mod bigrams;
-use bigrams::Bigrams;
+
+pub use anchor::Anchor;
 
 pub struct Scorer<'a> {
     re: Option<Regex>,
     term: &'a str,
-    term_bi: Bigrams,
-    line_bi: Bigrams,
+    anchor: Option<Anchor>,
+}
+
+#[derive(Debug)]
+pub struct Score {
+    pub regex: f32,
+    pub similarity: f32,
+}
+
+impl Score {
+    pub fn value(&self, min: f32, max: f32) -> u32 {
+        let regex = (self.regex - min) / (max - min);
+        let weighted = regex * 0.9 + self.similarity * 0.1;
+        (weighted * 100_000.0) as u32
+    }
 }
 
 impl<'a> Scorer<'a> {
-    pub fn new(term: &'a str) -> Self {
+    pub fn new(term: &'a str, anchor: Option<Anchor>) -> Self {
         Scorer {
             re: pattern(term),
             term,
-            term_bi: Bigrams::build(term),
-            line_bi: Bigrams::new(),
+            anchor,
         }
     }
 
-    pub fn score(&self, line: &str) -> u32 {
+    fn score(&mut self, line: &str) -> Option<Score> {
         if self.term.len() > line.len() {
-            return 0;
+            return None;
         }
 
-        self.re.as_ref().map_or(0, |re| {
-            re.find(line).map_or(0, |m| {
+        let anchor = &mut self.anchor;
+        self.re.as_ref().and_then(|re| {
+            re.find(line).and_then(|m| {
                 let range = m.end() - m.start() + 1;
                 let penalty = ((line.len() as f32) + 1.0) / 100.0;
                 let score = 1.0 / ((range as f32) + penalty);
-                (score * 100_000.0) as u32
+                Some(Score {
+                    regex: score,
+                    similarity: anchor.as_mut().map_or(0.0, |a| a.score(line)),
+                })
             })
         })
+    }
+
+    pub fn rank<I>(&mut self, lines: I) -> Vec<(String, Score)>
+    where
+        I: Iterator<Item = String>,
+    {
+        let mut min = 0.0;
+        let mut max = 0.0;
+
+        let mut matches: Vec<_> = lines
+            .filter_map(|line| match self.score(&line) {
+                None => None,
+                Some(val) => {
+                    if val.regex < min {
+                        min = val.regex;
+                    }
+                    if val.regex > max {
+                        max = val.regex;
+                    }
+                    Some((line, val))
+                }
+            })
+            .collect();
+
+        matches.sort_by(|a, b| a.1.value(min, max).cmp(&b.1.value(min, max)).reverse());
+        matches
     }
 }
 
@@ -69,67 +113,68 @@ mod tests {
 
     #[test]
     fn it_awards_no_points_for_empty_term() {
-        let mut scorer = Scorer::new("");
-        assert_eq!(0, scorer.score("hello"));
+        let mut scorer = Scorer::new("", None);
+        assert!(scorer.score("hello").is_none())
     }
 
     #[test]
     fn it_awards_no_points_for_empty_input() {
-        let mut scorer = Scorer::new("hello");
-        assert_eq!(0, scorer.score(""));
+        let mut scorer = Scorer::new("hello", None);
+        assert!(scorer.score("").is_none());
     }
 
     #[test]
     fn it_awards_no_points_for_term_longer_than_input() {
-        let mut scorer = Scorer::new("hello");
-        assert_eq!(0, scorer.score("hi"));
+        let mut scorer = Scorer::new("hello", None);
+        assert!(scorer.score("hi").is_none());
     }
 
     #[test]
     fn it_awards_more_points_for_exact_match_than_close_match() {
-        let mut scorer = Scorer::new("hello");
-        let a = scorer.score("hello");
-        let b = scorer.score("hellolo");
-        assert!(a > 0);
-        assert!(b > 0);
-        assert!(a > b);
+        let mut scorer = Scorer::new("hello", None);
+        let a = scorer.score("hello").unwrap();
+        let b = scorer.score("hellolo").unwrap();
+        assert!(a.regex > 0.0);
+        assert!(b.regex > 0.0);
+        assert!(a.regex > b.regex);
     }
 
     #[test]
     fn it_awards_more_points_for_shorter_match_range() {
-        let mut scorer = Scorer::new("hello");
-        let a = scorer.score("-hel-lo");
-        let b = scorer.score("hel--lo");
-        assert!(a > 0);
-        assert!(b > 0);
-        assert!(a > b);
+        let mut scorer = Scorer::new("hello", None);
+        let a = scorer.score("-hel-lo").unwrap();
+        let b = scorer.score("hel--lo").unwrap();
+        assert!(a.regex > 0.0);
+        assert!(b.regex > 0.0);
+        assert!(a.regex > b.regex);
     }
 
     #[test]
     fn it_awards_more_points_for_shorter_input() {
-        let mut scorer = Scorer::new("hello");
-        let a = scorer.score("-hello");
-        let b = scorer.score("--hello");
-        assert!(a > 0);
-        assert!(b > 0);
-        assert!(a > b);
+        let mut scorer = Scorer::new("hello", None);
+        let a = scorer.score("-hello").unwrap();
+        let b = scorer.score("--hello").unwrap();
+        assert!(a.regex > 0.0);
+        assert!(b.regex > 0.0);
+        assert!(a.regex > b.regex);
     }
 
     #[test]
     fn it_matches_case_insensitively() {
-        let mut scorer = Scorer::new("hello");
-        let a = scorer.score("Hello");
-        let b = scorer.score("hello");
-        assert!(a > 0);
-        assert_eq!(a, b);
+        let mut scorer = Scorer::new("hello", None);
+        let a = scorer.score("Hello").unwrap();
+        let b = scorer.score("hello").unwrap();
+        assert!(a.regex > 0.0);
+        assert_eq!(a.regex, b.regex);
     }
 
     #[test]
     fn it_matches_case_sensitively() {
-        let mut scorer = Scorer::new("Hello");
+        let mut scorer = Scorer::new("Hello", None);
         let a = scorer.score("Hello");
         let b = scorer.score("hello");
-        assert!(a > 0);
-        assert_eq!(0, b);
+        assert!(a.is_some());
+        assert!(b.is_none());
+        assert!(a.unwrap().regex > 0.0);
     }
 }
